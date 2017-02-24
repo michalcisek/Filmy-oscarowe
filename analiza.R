26d1c35ff823892c03d34cb5e711d89e69a86046
Michał Cisek <mc56283@sgh.waw.pl>
2017-02-24 15:52:23 +0100

    update

--- analiza.R
+++ analiza.R
@@ -2,6 +2,7 @@ rm(list=ls())
 df<-read.csv2("dane_oscar.csv")
 
 # Transformacja zmiennych -------------------------------------------------
+
 df$gross_ratio<-df$gross/df$budget
 df$ow_ratio<-df$opening_weekend/df$budget
 df$kom_zap_ratio<-df$komentarze/df$zewnetrzne_zapowiedzi
@@ -11,7 +12,9 @@ df$Oscar<-factor(df$Oscar)
 df$oscar_actor<-factor(df$oscar_actor)
 df$oscar_actor_binary<-factor(df$oscar_actor_binary)
 
-#dyskretyzacja budget, opening_weekend, gross, czas, rating, komentarze, zewnetrzne_zapowiedzi
+df$log_gross<-log(df$gross)
+df$log_budget<-log(df$budget)
+df$log_ow<-log(df$opening_weekend)
 
 df1<-df[complete.cases(df),]
 
@@ -20,35 +23,37 @@ df1<-df[complete.cases(df),]
 table(df1$Oscar)
 df1<-df1[-which(df1$Oscar==0 & df1$rok <2000),]
 
+var1<-df1[,c(2,5:11,13,16)]# defaultowa partia zmiennych objasniajacych
+var2<-df1[,c(2,8:11,13:17)] #dodane ratio
+var3<-df1[,c(2,8:11,13,16,18:20)]#bez ratio ale z logarytmami
+
 
 
 # Selekcja zmiennych ------------------------------------------------------
 
 library(Boruta)
-boruta<-Boruta(Oscar~. -Film -rok -Id -budget -gross -opening_weekend,df1)
+boruta1<-Boruta(Oscar~. -Film -Id -rok,df1)
 
-plot(boruta)
-zmienne_boruta<-getSelectedAttributes(boruta)
+plot(boruta1)
+
+boruta_formula1<-getConfirmedFormula(boruta1)
 
 test<-which(df1$rok==2016)
-df_trening<-df1[-test]
-df_test<-df1[test,]
 
 library(randomForest)
-rf1<-randomForest(x=df_trening[,zmienne_boruta],y=df_trening[,"Oscar"])
+set.seed(123)
+rf1<-randomForest(boruta_formula1,df1[-test,])
+
 plot(rf1)
-rf1<-randomForest(x=df_trening[,zmienne_boruta],y=df_trening[,"Oscar"],ntree=250)
 
 #534
-predict(rf1,df_test,type = "prob")
+predict(rf1,df1[test,],type = "prob")
 
 
-lr1<-glm(Oscar~. -Film -rok -Id -budget -gross -opening_weekend,df_trening,family = "binomial")
-lr2<-glm(Oscar~ czas+rating+komentarze+zewnetrzne_zapowiedzi+gross_ratio,df_trening,family = "binomial")
-predict(lr1,df_test,type="response")
-predict(lr2,df_test,type="response")
+lr1<-glm(Oscar~log_gross+log_budget+log_ow+oscar_actor_binary+gatunek+zewnetrzne_zapowiedzi+
+           komentarze+rating+czas,df1[-test,],family = "binomial")
+lr2<-step(lr1)
 
-
-
-
+predict(lr1,df1[test,],type="response")
+predict(lr2,df1[test,],type="response")
 
