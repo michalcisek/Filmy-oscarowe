f8dba028fe872d925adcf386af8bf5ebea8da89c
michalcisek <mc56283@sgh.waw.pl>
2017-02-26 13:13:18 +0100

    2016 prediction

--- analiza.R
+++ analiza.R
@@ -1,7 +1,24 @@
 rm(list=ls())
+library(datasets)
+library(GGally)
+library(ggplot2)
+
+my_fn <- function(data, mapping, ...){
+  p <- ggplot(data = data, mapping = mapping) + 
+    geom_point() + 
+    geom_smooth(method=loess, fill="red", color="red", ...) +
+    geom_smooth(method=lm, fill="blue", color="blue", ...)
+  p
+}
+
+# Zaladowanie danych ------------------------------------------------------
 df<-read.csv2("dane_oscar.csv")
 
 # Transformacja zmiennych -------------------------------------------------
+
+df$gross<-df$gross/1e5
+df$budget<-df$budget/1e5
+df$opening_weekend<-df$opening_weekend/1e5
 
 df$gross_ratio<-df$gross/df$budget
 df$ow_ratio<-df$opening_weekend/df$budget
@@ -12,48 +29,39 @@ df$Oscar<-factor(df$Oscar)
 df$oscar_actor<-factor(df$oscar_actor)
 df$oscar_actor_binary<-factor(df$oscar_actor_binary)
 
-df$log_gross<-log(df$gross)
-df$log_budget<-log(df$budget)
-df$log_ow<-log(df$opening_weekend)
 
 df1<-df[complete.cases(df),]
 
+test<-df1[which(df1$rok==2016),]
+df1<-df1[-which(df1$rok==2016),]
 
 #zbilansowanie zmiennej objaśnianej
 table(df1$Oscar)
-df1<-df1[-which(df1$Oscar==0 & df1$rok <2000),]
 
-var1<-df1[,c(2,5:11,13,16)]# defaultowa partia zmiennych objasniajacych
-var2<-df1[,c(2,8:11,13:17)] #dodane ratio
-var3<-df1[,c(2,8:11,13,16,18:20)]#bez ratio ale z logarytmami
+set.seed(56283)
+wygrane_filmy<-which(df1$Oscar==1)
+przegrane_filmy<-sample(which(df1$Oscar==0),50)
 
+df1<-df1[c(przegrane_filmy,wygrane_filmy),]
 
-
-# Selekcja zmiennych ------------------------------------------------------
-
-library(Boruta)
-boruta1<-Boruta(Oscar~. -Film -Id -rok,df1)
-
-plot(boruta1)
-
-boruta_formula1<-getConfirmedFormula(boruta1)
-
-test<-which(df1$rok==2016)
-
-library(randomForest)
-set.seed(123)
-rf1<-randomForest(boruta_formula1,df1[-test,])
-
-plot(rf1)
-
-#534
-predict(rf1,df1[test,],type = "prob")
+#Rocky jest outlierem jeśli chodzi o wielkosc opening_weekend (wygral Oscara)
+df1<-df1[-which(df1$ow_ratio==max(df1$ow_ratio)),]
 
 
-lr1<-glm(Oscar~log_gross+log_budget+log_ow+oscar_actor_binary+gatunek+zewnetrzne_zapowiedzi+
-           komentarze+rating+czas,df1[-test,],family = "binomial")
-lr2<-step(lr1)
+# Wizualizacja zaleznosci miedzy zmiennymi --------------------------------
 
-predict(lr1,df1[test,],type="response")
-predict(lr2,df1[test,],type="response")
+# g = ggpairs(df1,columns = c(2,7:12,14:17), lower = list(continuous = my_fn))
+#gross, czas, rating, zewnetrzne_zapowiedzi i kom_zap_ratio wydaja sie najsensowniejszymi kandydatami na zmienne objasniajace
+# g
 
+# Modelowanie ------------------------------------------------------
+
+lr1<-glm(Oscar~gross+czas+rating+zewnetrzne_zapowiedzi+kom_zap_ratio,df1,family = "binomial")
+summary(lr1)
+
+# La la land głównym kandydatem na zwycięzce...
+predykcja_lr<-predict(lr1,test,type="response")
+
+# ... co zgadza sie z kursami buckmacherskimi
+final<-data.frame(film=test[,1],predykcja_lr,bukcmacher_kurs=c(100,100,100,100,21,1.14,50,30,5))
+View(final[order(-final$predykcja_lr),])
